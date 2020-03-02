a4ccf39f5593432f5f4a31557a33d2b47d8f5013
Tom <tom@bearjam.dev>
2020-03-02 11:13:09 +0000

    added emotion, tailwind

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import { css } from '@emotion/core';
+import tw from 'tailwind.macro';
 
 import Header from './header'
 import './layout.css'
+
+const divStyles = [
+  tw`my-0 mx-auto pt-0 px-4 pb-6 max-w-3xl`,
+  css`background-color: pink;`
+];
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,14 +26,7 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div css={divStyles}>
           {children}
         </div>
       </>
